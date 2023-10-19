813056c9c28ab1871fae99a0ccfa5e3a4fb785fc
Khant <khantthwethwesan.edka@gmail.com>
2023-10-19 08:33:17 +0630

    fix: config

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,10 +3,10 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import { Footer, Navbar } from '../components';
 
-const inter = Inter({ subsets: ['latin'] })
+
 
 export const metadata: Metadata = {
-  title: 'Metaverse',
+  title: 'Travlog',
   description: 'Lets explore the world',
 }
 
